97155ab61cfa6d0c55b3f8b262fee17a0ae7641e
Augustus <99241183+ZeroFuruya@users.noreply.github.com>
2025-03-18 22:49:08 +0800

    trying_stuffs, debugging lesson exercise done

---
+++ own_practice/trying_stuffs.rb
@@ -0,0 +1,16 @@
+# rubocop:disable Style/FrozenStringLiteralComment
+require 'pry-byebug'
+
+def isogram?(string)
+  original_length = string.length
+  string_array = string.downcase.split
+
+  binding.pry
+
+  unique_length = string_array.uniq.length
+  original_length == unique_length
+end
+
+isogram?("Odin")
+
+# rubocop:enable Style/FrozenStringLiteralComment
